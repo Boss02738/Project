31a03da3331af6a8e87ac1a4f116c6bedae0e83d
Boss02738 <boss02738@hotmail.com>
2025-11-17 21:52:30 +0700

    แก้ถอน

--- my_note_app/lib/services/socket_service.dart
+++ my_note_app/lib/services/socket_service.dart
@@ -1,3 +1,35 @@
+// import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+// class SocketService {
+//   static final SocketService _i = SocketService._internal();
+//   factory SocketService() => _i;
+//   SocketService._internal();
+
+//   IO.Socket? _socket;
+
+//   void connect(String baseUrl) {
+//     _socket ??= IO.io(baseUrl, IO.OptionBuilder()
+//         .setTransports(['websocket'])
+//         .disableAutoConnect()
+//         .build());
+//     if (!(_socket?.connected ?? false)) _socket!.connect();
+//   }
+
+//   void joinUserRoom(int userId) {
+//     _socket?.emit('join_user', {'userId': userId});
+//   }
+
+//   void onNotify(void Function(dynamic data) handler) {
+//     _socket?.on('notify', handler);
+//   }
+
+//   void dispose() {
+//     _socket?.dispose();
+//     _socket = null;
+//   }
+// }
+
+// lib/services/socket_service.dart
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
@@ -8,19 +40,24 @@ class SocketService {
   IO.Socket? _socket;
 
   void connect(String baseUrl) {
-    _socket ??= IO.io(baseUrl, IO.OptionBuilder()
-        .setTransports(['websocket'])
-        .disableAutoConnect()
-        .build());
+    _socket ??= IO.io(
+      baseUrl,
+      IO.OptionBuilder()
+          .setTransports(['websocket'])
+          .disableAutoConnect()
+          .build(),
+    );
     if (!(_socket?.connected ?? false)) _socket!.connect();
   }
 
   void joinUserRoom(int userId) {
-    _socket?.emit('join_user', {'userId': userId});
+    // ให้ตรงกับ server: socket.on("register", ...)
+    _socket?.emit('register', userId);
   }
 
   void onNotify(void Function(dynamic data) handler) {
-    _socket?.on('notify', handler);
+    // ให้ตรงกับ notificationController: io.to(...).emit("notification:new", item)
+    _socket?.on('notification:new', handler);
   }
 
   void dispose() {
